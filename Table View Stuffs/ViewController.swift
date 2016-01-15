b2da5453b83aef42d7941243a2318726f950282d
Jose Ramon Soto <student@Joses-MacBook-Air.local>
2016-01-15 11:06:09 -0600

    tableview editing features

--- Table View Stuffs/ViewController.swift
+++ Table View Stuffs/ViewController.swift
@@ -13,6 +13,29 @@ var realNames = ["Master N8", "Diana", "Bruce Wayne", "Barry Allen", "Steve", "A
         myTableView.dataSource = self
         myTableView.delegate = self
     }
+    @IBAction func addButtonTapped(sender: UIBarButtonItem)
+    {
+        let myAlert = UIAlertController(title: "Add Superhero", message: nil, preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        myAlert.addAction(cancelAction)
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (addAction) -> Void in
+            let superheroNameTextField = myAlert.textFields![0]
+            as UITextField
+            let aliasTextField = myAlert.textFields![1] as UITextField
+            self.superheros.append(superheroNameTextField.text!)
+            self.realNames.append(aliasTextField.text!)
+            self.myTableView.reloadData()
+        }
+        myAlert.addAction(addAction)
+        
+        myAlert.addTextFieldWithConfigurationHandler { (superHeroTextField) -> Void in
+            superHeroTextField.placeholder = "Add Superhero Name"
+        }
+        myAlert.addTextFieldWithConfigurationHandler { (liasTextField) -> Void in
+            aliasTextField.placeholder = "Add Real Name"
+        //presents the alert view controller
+        self.presentViewController(myAlert, animated: true, completion: nil)
+    }
     
     //creates a cell that will store data
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
@@ -28,7 +51,16 @@ var realNames = ["Master N8", "Diana", "Bruce Wayne", "Barry Allen", "Steve", "A
     {
         return superheros.count
     }
-    
+//Allows you to delete rows from table view
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == .Delete
+        {
+            superheros.removeAtIndex(indexPath.row)
+            realNames.removeAtIndex(indexPath.row)
+            myTableView.reloadData()
+        }
+    }
 }
 
 
